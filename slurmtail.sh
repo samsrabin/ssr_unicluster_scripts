0066627bd844f3e74166318abb08ef6bbc98a486
Sam Rabin <sam.rabin@kit.edu>
2021-03-18 12:18:01 +0100

    Added "options" argument for multitail in slurmtail.sh.

--- slurmtail.sh
+++ slurmtail.sh
@@ -7,14 +7,16 @@ if [[ "${jobid}" == "" ]]; then
 	exit 1
 fi
 
+options="${2}"
+
 jobinfo=$(scontrol show job $jobid)
 file_out=$(echo $jobinfo | grep -oE "StdOut=\S+" | sed "s/StdOut=//g")
 file_err=$(echo $jobinfo | grep -oE "StdErr=\S+" | sed "s/StdErr=//g")
 
 if [[ "${file_out}" == "${file_err}" ]]; then
-	tail -F -n 100 "${file_out}"
+	multitail -s 2 --follow-all --retry-all -n 100  ${options} -i "${file_out}"
 else
-	multitail -s 2 --follow-all --retry-all -n 100 -i "${file_out}" -i "${file_err}"
+	multitail -s 2 --follow-all --retry-all -n 100  ${options} -i "${file_out}" -i "${file_err}"
 fi
 
 exit 0
