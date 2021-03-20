3e4b9f80830970da0caac4a132ba91cc06faba9b
Sam Rabin <sam.rabin@kit.edu>
2021-03-20 16:03:54 +0100

    Added failcellls.sh

---
+++ failcells.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+set -e
+
+insfile=$(grep -E "^\s*mpirun\s" submit.sh | grep -oE "\S+$")
+gl=$(get_param.sh ${insfile} "file_gridlist")
+nparts=$(ls -d run*/ | grep -oE "[0-9]+" | sort -g | tail -n 1)
+
+stdoutfile=$(ls -tr guess_x.o* | tail -n 1)
+stderrfile=$(ls -tr guess_x.e* | tail -n 1)
+lastjob=$(echo "${stdoutfile}" | grep -oE "[0-9]+")
+thisdir=logs-${lastjob}
+echo ${thisdir}/
+mkdir -p ${thisdir}
+cp guess_x.*${lastjob} ${thisdir}/
+cp guess ${thisdir}/
+cp latest* ${thisdir}/
+cp submit.sh ${thisdir}/
+cp *ins ${thisdir}/
+cp ${gl} ${thisdir}/
+cd ${thisdir}
+
+gl_failcells=failcells.txt
+gl_notruncells=notruncells.txt
+gl_minusfailed=gridlist_minusfailed.txt
+for f in ${gl_failcells} ${gl_notruncells}; do
+   [[ -f $f ]] && rm $f
+   touch $f
+done
+cp ../${gl} ${gl_minusfailed}
+
+# Get logs
+echo "   Getting logs..."
+mkdir -p logs
+for x in $(seq 0 $((nparts - 1))); do
+   thisout="logs/run$((x+1)).out"
+   thiserr="logs/run$((x+1)).err"
+   if [[ ! -f "${thisout}" ]]; then
+      grep "\[1,$x\]" ${stdoutfile} > "${thisout}"
+   fi
+   if [[ ! -f "${thiserr}" ]]; then
+      grep "\[1,$x\]" ${stderrfile} > "${thiserr}"
+   fi
+done
+
+# Get list of failed cells
+failranks=$(grep -i "invalid\|fail\|error" ${stdoutfile} | grep -oE "\[[0-9]+,[0-9]+\]" | sed -e "s/\[[0-9]\+,//" | sed "s/\]//")
+if [[ "${failranks}" != "" ]]; then
+   echo "   Getting ${gl_failcells} and ${gl_minusfailed}..."
+   for r in ${failranks}; do
+      tmp=$(grep "Commenc" logs/run$((r+1)).log | tail -n 1 | grep -oE " at .*$" | grep -oE "[-0-9\.]+,[-0-9\.]+" | sed "s/(\|)//g" | sed "s/,/ /")
+      echo $tmp >> ${gl_failcells}
+      sed -i "/${tmp}/d" ${gl_minusfailed}   
+   done
+fi
+
+# Get not-run cells
+if [[ $(cat "${gl_failcells}" | wc -l ) -gt 0 ]]; then
+   echo "   Getting ${gl_notruncells}..."
+   for r in $(seq 1 ${nparts}); do
+      set +e
+      grep "Finished" logs/run${r}.log >/dev/null
+      if [[ $? -eq 0 ]]; then
+         continue
+      fi
+      set -e
+      g0=$(grep "Commenc" logs/run${r}.log | tail -n 1 | grep -oE " at .*$" | grep -oE "[-0-9\.]+,[-0-9\.]+" | sed "s/(\|)//g" | sed "s/,/ /")
+      grep -A 999999 "${g0/-/\\-}" ../run${r}/${gl} >> ${gl_notruncells}
+   done
+   echo "   Removing failed cells..."
+   for r in ${failranks}; do
+      tmp=$(grep "Commenc" logs/run$((r+1)).log | tail -n 1 | grep -oE " at .*$" | grep -oE "[-0-9\.]+,[-0-9\.]+" | sed "s/(\|)//g" | sed "s/,/ /")
+      sed -i "/${tmp}/d" ${gl_notruncells}   
+   done
+fi
+
+echo " "
+echo "Results:"
+wc -l ${gl} | sed "s/ /\t/"
+wc -l ${gl_failcells} | sed "s/ /\t/"
+wc -l ${gl_notruncells} | sed "s/ /\t/"
+wc -l ${gl_minusfailed} | sed "s/ /\t/"
+
+exit 0
+
+
+
+
+
+
+
+
