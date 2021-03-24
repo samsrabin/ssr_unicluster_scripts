f4ade25fa94431bacfb1d1a07ec35fdaee43981d
Sam Rabin <sam.rabin@kit.edu>
2021-03-24 13:08:37 +0100

    Added g2p_get_basename.sh

---
+++ g2p_get_basename.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Find the first directory above (or including) this one that is four_under-score_separated_words 
+thisbasename=$(echo $PWD | { grep -oE "/[a-zA-Z0-9\-]+_[a-zA-Z0-9\-]+_[a-zA-Z0-9\-]+_[a-zA-Z0-9\-]+/" || true; } | tail -n 1 | sed "s@/@@g")
+
+if [[ "${thisbasename}" == "" ]]; then
+	>&2 echo "g2p_get_basename.sh: No matches found"
+	>&2 echo PWD $PWD
+	exit 1
+fi
+
+echo $thisbasename
+
+exit 0
