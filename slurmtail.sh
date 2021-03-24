c7d18359e6f9000de1b49eafa58f6088a963710f
Sam Rabin <sam.rabin@kit.edu>
2021-03-24 13:49:25 +0100

    Added slurmtail.sh support for completed jobs.

--- slurmtail.sh
+++ slurmtail.sh
@@ -9,9 +9,26 @@ fi
 
 options="${2}"
 
-jobinfo=$(scontrol show job $jobid)
-file_out=$(echo $jobinfo | grep -oE "StdOut=\S+" | sed "s/StdOut=//g")
-file_err=$(echo $jobinfo | grep -oE "StdErr=\S+" | sed "s/StdErr=//g")
+jobinfo=$({ scontrol show job $jobid 2>/dev/null || true; })
+if [[ "${jobinfo}" == "" ]]; then
+	workdir=$(sacct -j $jobid -o WorkDir%1000 | grep -v "WorkDir\|\-\-\-" | grep -E "\S+" | sed -E "s@^\s+/@/@" | sed -E "s@ \$@@")
+	if [[ ! -e "${workdir}" ]]; then
+		echo "workdir not found: $workdir"
+		exit 1
+	fi
+	file_out="${workdir}/guess_x.o$jobid"
+	if [[ ! -e "${file_out}" ]]; then
+		echo "file_out not found: $file_out"
+		exit 1
+	fi
+	file_err="${workdir}/guess_x.e$jobid"
+	if [[ ! -e "${file_err}" ]]; then
+		file_err="${file_out}"
+	fi
+else
+	file_out=$(echo $jobinfo | grep -oE "StdOut=\S+" | sed "s/StdOut=//g")
+	file_err=$(echo $jobinfo | grep -oE "StdErr=\S+" | sed "s/StdErr=//g")
+fi
 
 if [[ "${file_out}" == "${file_err}" ]]; then
 	multitail -s 2 --follow-all --retry-all -n 100  ${options} -i "${file_out}"
