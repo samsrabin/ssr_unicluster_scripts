7e9045e2ccd3275c2c9040bbef3df9d2bd1c2e8c
Sam Rabin <sam.rabin@kit.edu>
2021-03-28 13:56:57 +0200

    slurmtail.sh can now accept a job name snippet. It will show the top-numbered matching RUNNING/COMPLETING job. If none, it will show the top-numbered PENDING job.

--- slurmtail.sh
+++ slurmtail.sh
@@ -6,7 +6,7 @@ initn=100
 
 jobid=$1
 if [[ "${jobid}" == "" ]]; then
-	echo "You must provide a job ID"
+	echo "You must provide a job ID or jobname string"
 	exit 1
 fi
 
@@ -15,20 +15,35 @@ options="$@"
 
 jobinfo=$({ scontrol show job $jobid 2>/dev/null || true; })
 if [[ "${jobinfo}" == "" ]]; then
-	workdir=$(sacct -j $jobid -o WorkDir%1000 | grep -v "WorkDir\|\-\-\-" | grep -E "\S+" | sed -E "s@^\s+/@/@" | sed -E "s@ \$@@")
+	workdir=$(sacct -j $jobid -o WorkDir%1000 2>/dev/null | grep -v "WorkDir\|\-\-\-" | grep -E "\S+" | sed -E "s@^\s+/@/@" | sed -E "s@ \$@@")
 	if [[ ! -e "${workdir}" ]]; then
-		echo "workdir not found: $workdir"
-		exit 1
-	fi
-	file_out="${workdir}/guess_x.o$jobid"
-	if [[ ! -e "${file_out}" ]]; then
-		echo "file_out not found: $file_out"
-		exit 1
-	fi
-	file_err="${workdir}/guess_x.e$jobid"
-	if [[ ! -e "${file_err}" ]]; then
-		file_err="${file_out}"
-	fi
+        jobs=$({ squeue -o "%i %j %T %r" | sort | grep "${jobid}" || true; })
+        if [[ "${jobs}" == "" ]]; then
+            echo "No matching job number or name found"
+            exit 1
+        fi
+        thisjob=$({ echo "${jobs}" | grep "RUNNING\|COMPLETING" | head -n 1 | cut -d' ' -f1 || true; })
+        if [[ "${thisjob}" == "" ]]; then
+            thisjob=$({ echo "${jobs}" | grep "PENDING" | head -n 1 | cut -d' ' -f1 || true; })
+            if [[ "${thisjob}" == "" ]]; then
+                echo "No matching job number found. Matching job name(s), but state not recognized" 
+                echo "${jobs}"
+            fi
+        fi
+        jobinfo=$({ scontrol show job $thisjob 2>/dev/null || true; })
+	    file_out=$(echo $jobinfo | grep -oE "StdOut=\S+" | sed "s/StdOut=//g")
+	    file_err=$(echo $jobinfo | grep -oE "StdErr=\S+" | sed "s/StdErr=//g")
+    else
+    	file_out="${workdir}/guess_x.o$jobid"
+    	if [[ ! -e "${file_out}" ]]; then
+    		echo "file_out not found: $file_out"
+    		exit 1
+    	fi
+    	file_err="${workdir}/guess_x.e$jobid"
+    	if [[ ! -e "${file_err}" ]]; then
+    		file_err="${file_out}"
+    	fi
+    fi
 else
 	file_out=$(echo $jobinfo | grep -oE "StdOut=\S+" | sed "s/StdOut=//g")
 	file_err=$(echo $jobinfo | grep -oE "StdErr=\S+" | sed "s/StdErr=//g")
