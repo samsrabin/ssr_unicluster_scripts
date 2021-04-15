cff146ec6a321778bf64e115e330b8396c5bf92c
Sam Rabin <sam.rabin@kit.edu>
2021-04-15 11:05:25 +0200

    Added script for getting all ins-files imported by a given ins-file or its imports.

---
+++ get_ins_dependencies.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+set -e
+
+# Name of this ins-file
+insfile=$1
+if [[ ! -e "${insfile}" ]]; then
+   >&2 echo "get_param.sh: ${insfile} not found!"
+   exit 1
+elif [[ "${insfile}" == "" ]]; then
+   >&2 echo "get_param.sh: You must provide a top insfile"
+   exit 1
+fi
+
+# Change to the directory
+cd $(dirname ${insfile})
+insfile=$(basename "${insfile}")
+
+# Find all lines beginning (after any spaces) with:
+#    import "somefile.ins"
+# IFS business makes it so that the elements of the array are distinguished by newlines rather than spaces
+IFS_backup=$IFS
+IFS=$'\n'
+set +e
+results=($(grep -E "^\s*import" ${insfile}))
+set -e
+IFS=$IFS_backup
+
+if [[ "${results}" != "" ]]; then
+for (( idx=${#results[@]}-1 ; idx>=0 ; idx-- )) ; do
+    thisline="${results[idx]}"
+    f=$(echo $thisline | sed -E 's@^\s*import "(\S+.ins)".*@\1@')
+    f_dependencies="$(get_ins_dependencies.sh $f)"
+    if [[ "${f_dependencies}" != "" ]]; then
+        dependencies="$dependencies $f_dependencies"
+    fi
+    dependencies="$dependencies $f"
+done
+fi
+
+echo $dependencies
+
+
+exit 0
