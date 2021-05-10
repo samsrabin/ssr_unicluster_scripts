7c696ac58f1a258783baecc4d54c3193adafb5e3
Sam Rabin <sam.rabin@kit.edu>
2021-05-10 10:49:37 +0200

    Standardized indentation in g2p_view_jobchains.sh to 4 spaces.

--- g2p_view_jobchains.sh
+++ g2p_view_jobchains.sh
@@ -31,10 +31,10 @@ capture() { eval "$(_capture "$@")"; }
 
 
 if [[ $testing -eq 0 ]]; then
-	jobs=$(squeue -o "%P %i %j %T %r")
+    jobs=$(squeue -o "%P %i %j %T %r")
 else
-	# Note: { grep ... || true; } ensures that no error occurs if grep finds no matches
-	jobs=$(squeue -o "%P %i %j %T %r" | { grep "dev_sin" || true; })
+    # Note: { grep ... || true; } ensures that no error occurs if grep finds no matches
+    jobs=$(squeue -o "%P %i %j %T %r" | { grep "dev_sin" || true; })
 fi
 
 #function string_contains {
@@ -46,189 +46,189 @@ fi
 #}
 
 function was_canceled {
-	jobnum=$1
+    jobnum=$1
 
-	# Check whether we've already sacct'd this run; if not, do so
-	sacct_file=sacct.${jobnum}
-	if [[ ! -e ${sacct_file} ]]; then
-		sacct -n -j $jobnum > ${sacct_file}
-	fi
+    # Check whether we've already sacct'd this run; if not, do so
+    sacct_file=sacct.${jobnum}
+    if [[ ! -e ${sacct_file} ]]; then
+        sacct -n -j $jobnum > ${sacct_file}
+    fi
 
-	sacct_result="$(cat ${sacct_file})"
-	if [[ "${sacct_result}" == "" ]]; then
-		echo -1
-	else
-		echo "${sacct_result}" | grep "CANCEL" | wc -l
-	fi
+    sacct_result="$(cat ${sacct_file})"
+    if [[ "${sacct_result}" == "" ]]; then
+        echo -1
+    else
+        echo "${sacct_result}" | grep "CANCEL" | wc -l
+    fi
 }
 
 function get_symbol_() { passback latest_job; }
 
 function get_symbol() {
-	# First, change to working directory
-	if [[ ! -d "${homedir_rel}" ]]; then
-		>&2 echo "homedir_rel not found: ${homedir_rel}"
+    # First, change to working directory
+    if [[ ! -d "${homedir_rel}" ]]; then
+        >&2 echo "homedir_rel not found: ${homedir_rel}"
         >&2 echo "pwd: $PWD"
-		exit 14
-	fi
-	if [[ $testing -eq 0 ]]; then 
-		workdir=$(realpath "${homedir_rel}" | sed "s@/pfs/data5@@" | sed "s@$HOME@$WORK@")
-	else
-		workdir=$(realpath $(echo "${homedir_rel}" | sed "s@/@_test/@") | sed "s@/pfs/data5@@" | sed "s@$HOME@$WORK@")
-	fi
+        exit 14
+    fi
+    if [[ $testing -eq 0 ]]; then 
+        workdir=$(realpath "${homedir_rel}" | sed "s@/pfs/data5@@" | sed "s@$HOME@$WORK@")
+    else
+        workdir=$(realpath $(echo "${homedir_rel}" | sed "s@/@_test/@") | sed "s@/pfs/data5@@" | sed "s@$HOME@$WORK@")
+    fi
 
-	# workdir does NOT exist
-	if [[ ! -d "${workdir}" ]]; then
-		symbol="${symbol_norun}"
+    # workdir does NOT exist
+    if [[ ! -d "${workdir}" ]]; then
+        symbol="${symbol_norun}"
 
-	# workdir DOES exist
-	else
-		pushd "${workdir}"
-		workdir_short=$(echo $workdir | sed "s@${WORK}@\$WORK@")
-	
-		matching_jobs=$(echo "${jobs}" | { grep -E "\s${thepattern}" || true; })
+        # workdir DOES exist
+    else
+        pushd "${workdir}"
+        workdir_short=$(echo $workdir | sed "s@${WORK}@\$WORK@")
 
-		# There are NO matching jobs
-	   if [[ "${matching_jobs}" == "" ]]; then
+        matching_jobs=$(echo "${jobs}" | { grep -E "\s${thepattern}" || true; })
 
-			# SIMULATION
-			if [[ "${thepattern:0:6}" != "jobfin" ]]; then
-				latest_job=$(grep "LPJ-GUESS run" latest_submitted_jobs.log | awk 'END {print $NF}')
+        # There are NO matching jobs
+        if [[ "${matching_jobs}" == "" ]]; then
 
-				# If no run was started in this chain, then say so
-				if [[ ${latest_job} -lt ${latest_actual_job} ]]; then
-					symbol="${symbol_norun}"
+            # SIMULATION
+            if [[ "${thepattern:0:6}" != "jobfin" ]]; then
+                latest_job=$(grep "LPJ-GUESS run" latest_submitted_jobs.log | awk 'END {print $NF}')
 
-				# Otherwise, check if simulation began
-				else
-					file_stdout="guess_x.o${latest_job}"
-					# If not, assume it was canceled before beginning.
-			        if [[ ! -e "${file_stdout}" ]]; then
-						was_it_canceled=$(was_canceled ${latest_job})
-						if [[ ${was_it_canceled} -lt 0 ]]; then
-							symbol="${symbol_unknown2}"
-						elif [[ ${was_it_canceled} -eq 0 ]]; then
-							symbol="${symbol_unknown}"
-						else
-							symbol="${symbol_canceled_manual}"
-						fi
-                    # Otherwise, check if we've already processed this stdout
+                # If no run was started in this chain, then say so
+                if [[ ${latest_job} -lt ${latest_actual_job} ]]; then
+                    symbol="${symbol_norun}"
+
+                    # Otherwise, check if simulation began
+                else
+                    file_stdout="guess_x.o${latest_job}"
+                    # If not, assume it was canceled before beginning.
+                    if [[ ! -e "${file_stdout}" ]]; then
+                        was_it_canceled=$(was_canceled ${latest_job})
+                        if [[ ${was_it_canceled} -lt 0 ]]; then
+                            symbol="${symbol_unknown2}"
+                        elif [[ ${was_it_canceled} -eq 0 ]]; then
+                            symbol="${symbol_unknown}"
+                        else
+                            symbol="${symbol_canceled_manual}"
+                        fi
+                        # Otherwise, check if we've already processed this stdout
                     elif [[ -e ${file_stdout}.ok ]]; then
                         symbol="${symbol_ok}"
                     elif [[ -e ${file_stdout}.canceled_manual ]]; then
                         symbol="${symbol_canceled_manual}"
                     elif [[ -e ${file_stdout}.fail ]]; then
                         symbol="${symbol_failed}"
-					# Otherwise...
-					else
-						# If all cells completed with "Finished" message, that's great!
+                        # Otherwise...
+                    else
+                        # If all cells completed with "Finished" message, that's great!
                         nprocs=$({ head "${file_stdout}" | grep -oE "Total slots allocated [0-9]+" | grep -oE "[0-9]+" || true; })
                         if [[ "${nprocs}" == "" ]]; then
                             >&2 echo "Error getting nprocs"
                             exit 17
                         fi
-						nfinished=$(grep "Finished" ${file_stdout} | grep -v "Finished with" | wc -l )
-				        nunfinished=$((nprocs - nfinished))
-						if [[ $nprocs == $nfinished ]]; then
-							symbol="${symbol_ok}"
+                        nfinished=$(grep "Finished" ${file_stdout} | grep -v "Finished with" | wc -l )
+                        nunfinished=$((nprocs - nfinished))
+                        if [[ $nprocs == $nfinished ]]; then
+                            symbol="${symbol_ok}"
                             touch ${file_stdout}.ok
-			
-						# If not, was job canceled?
-						elif [[ $(tail -n 100 ${file_stdout} | grep "State: CANCELLED" | wc -l) -ne 0 ]]; then
-							symbol="${symbol_canceled_manual}"
+
+                            # If not, was job canceled?
+                        elif [[ $(tail -n 100 ${file_stdout} | grep "State: CANCELLED" | wc -l) -ne 0 ]]; then
+                            symbol="${symbol_canceled_manual}"
                             touch ${file_stdout}.canceled_manual
-			
-						# Otherwise, assume run failed.
-						else
+
+                            # Otherwise, assume run failed.
+                        else
                             >&2 echo $nprocs
                             >&2 echo $nfinished
-							symbol="${symbol_failed}"
+                            symbol="${symbol_failed}"
                             touch ${file_stdout}.fail
-						fi
+                        fi
 
-					fi # Was it canceled before beginning?
-				fi # Was a run started in this chain?
-	
-			# JOBFIN
-			else
-				latest_job=$(grep "job_finish" latest_submitted_jobs.log | awk 'END {print $NF}')
-				# If no run was started in this chain, then say so
-				if [[ ${latest_job} -lt ${latest_actual_job} ]]; then
-					symbol="${symbol_norun}"
+                    fi # Was it canceled before beginning?
+                fi # Was a run started in this chain?
 
-				# Otherwise, check if simulation began
-				else
-					file_stdout="job_finish.${latest_job}.log"
-					if [[ ! -e "${file_stdout}" ]]; then
-						was_it_canceled=$(was_canceled ${latest_job})
-						if [[ ${was_it_canceled} -lt 0 ]]; then
-							symbol="${symbol_unknown2}"
-							>&2 echo "${symbol} stdout file not found: ${workdir_short}/${file_stdout}"
-						elif [[ ${was_it_canceled} -eq 0 ]]; then
-							symbol="${symbol_unknown}"
-							>&2 echo "${symbol} stdout file not found: ${workdir_short}/${file_stdout}"
-						else
-							symbol="${symbol_canceled_manual}"
-						fi
-					else
-		
-						# Completed successfully?
-						if [[ $(tail -n 20 ${file_stdout} | grep "All done\!" | wc -l) -gt 0 ]]; then
-							symbol="${symbol_ok}"
-	
-						# If not, was job canceled automatically?
-						elif [[ $(head ${file_stdout} | grep "Canceling because" | wc -l) -gt 0 ]]; then
-			            symbol="${symbol_canceled_auto}"
-			
-			         # If not, was job canceled manually?
-			         elif [[ $(tail -n 100 ${file_stdout} | grep "State: CANCELLED" | wc -l) -ne 0 ]]; then
-			            symbol="${symbol_canceled_manual}"
-			
-			         # Otherwise, assume run failed.
-			         else
-			            symbol="${symbol_failed}"
-			         fi
-					fi # Does stdout file exist?
-				fi # Was a run started in this chain?	
-			fi # Is it a simulation or jobfin?
-	
-		# There ARE matching jobs
-		else
-			latest_job=$(echo ${matching_jobs} | cut -d' ' -f2)
-			status=$(echo ${matching_jobs} | cut -d' ' -f4)
-			if [[ "${status}" == "PENDING" ]]; then
-				if [[ $(echo ${matching_jobs} | grep "Dependency" | wc -l) -gt 0 ]]; then
-					symbol="${symbol_pend_depend}"
-				else
-					symbol="${symbol_pend_other}"
-				fi
-			elif [[ "${status}" == "CONFIGURING" || "${status}" == "RUNNING" || "${status}" == "COMPLETING" ]]; then
-			   symbol="${symbol_running}"
-			else
-				>&2 echo ${matching_jobs}
-			   >&2 echo "status ${status} not recognized"
-			   exit 2
-			fi # What's the job status?
-		fi # Are there matching jobs?
-		popd
-	fi # Does workdir exist?
+                # JOBFIN
+            else
+                latest_job=$(grep "job_finish" latest_submitted_jobs.log | awk 'END {print $NF}')
+                # If no run was started in this chain, then say so
+                if [[ ${latest_job} -lt ${latest_actual_job} ]]; then
+                    symbol="${symbol_norun}"
 
-	echo $symbol
+                    # Otherwise, check if simulation began
+                else
+                    file_stdout="job_finish.${latest_job}.log"
+                    if [[ ! -e "${file_stdout}" ]]; then
+                        was_it_canceled=$(was_canceled ${latest_job})
+                        if [[ ${was_it_canceled} -lt 0 ]]; then
+                            symbol="${symbol_unknown2}"
+                            >&2 echo "${symbol} stdout file not found: ${workdir_short}/${file_stdout}"
+                        elif [[ ${was_it_canceled} -eq 0 ]]; then
+                            symbol="${symbol_unknown}"
+                            >&2 echo "${symbol} stdout file not found: ${workdir_short}/${file_stdout}"
+                        else
+                            symbol="${symbol_canceled_manual}"
+                        fi
+                    else
+
+                        # Completed successfully?
+                        if [[ $(tail -n 20 ${file_stdout} | grep "All done\!" | wc -l) -gt 0 ]]; then
+                            symbol="${symbol_ok}"
+
+                            # If not, was job canceled automatically?
+                        elif [[ $(head ${file_stdout} | grep "Canceling because" | wc -l) -gt 0 ]]; then
+                            symbol="${symbol_canceled_auto}"
+
+                            # If not, was job canceled manually?
+                        elif [[ $(tail -n 100 ${file_stdout} | grep "State: CANCELLED" | wc -l) -ne 0 ]]; then
+                            symbol="${symbol_canceled_manual}"
+
+                            # Otherwise, assume run failed.
+                        else
+                            symbol="${symbol_failed}"
+                        fi
+                    fi # Does stdout file exist?
+                fi # Was a run started in this chain?	
+            fi # Is it a simulation or jobfin?
+
+            # There ARE matching jobs
+        else
+            latest_job=$(echo ${matching_jobs} | cut -d' ' -f2)
+            status=$(echo ${matching_jobs} | cut -d' ' -f4)
+            if [[ "${status}" == "PENDING" ]]; then
+                if [[ $(echo ${matching_jobs} | grep "Dependency" | wc -l) -gt 0 ]]; then
+                    symbol="${symbol_pend_depend}"
+                else
+                    symbol="${symbol_pend_other}"
+                fi
+            elif [[ "${status}" == "CONFIGURING" || "${status}" == "RUNNING" || "${status}" == "COMPLETING" ]]; then
+                symbol="${symbol_running}"
+            else
+                >&2 echo ${matching_jobs}
+                >&2 echo "status ${status} not recognized"
+                exit 2
+            fi # What's the job status?
+        fi # Are there matching jobs?
+        popd
+    fi # Does workdir exist?
+
+    echo $symbol
 }
 
 function check_jobs {
 
-	# Model run
-	thepattern="${1}"
-	capture newpart get_symbol "${thisstatus}"
-	thisline="${thisline} ${newpart}"
-#	echo $thisline
-	
-	# Postprocessing
-	thepattern="jobfin_${thepattern}"
-	capture newpart get_symbol "${thisstatus}"
-	thisline="${thisline}/${newpart}"
-#	echo $thisline
+    # Model run
+    thepattern="${1}"
+    capture newpart get_symbol "${thisstatus}"
+    thisline="${thisline} ${newpart}"
+    #	echo $thisline
+
+    # Postprocessing
+    thepattern="jobfin_${thepattern}"
+    capture newpart get_symbol "${thisstatus}"
+    thisline="${thisline}/${newpart}"
+    #	echo $thisline
 
 }
 
@@ -241,56 +241,56 @@ dirlist=$(ls | grep -v "calibration\|_test")
 act_col_heads=""
 pot_col_heads=""
 for d in ${dirlist}; do
-	islast_act=0
+    islast_act=0
 
-	# If this directory doesn't even have a working directory set up, you can skip
-	thischain_workdir=$(echo $d | sed "s@/pfs/data5@@" | sed "s@$HOME@$WORK@")
-	if [[ ${testing} -eq 1 ]]; then
-		thischain_workdir="${thischain_workdir}_test"
-	fi
-	if [[ ! -d ${thischain_workdir} ]]; then
-		continue
-	fi
+    # If this directory doesn't even have a working directory set up, you can skip
+    thischain_workdir=$(echo $d | sed "s@/pfs/data5@@" | sed "s@$HOME@$WORK@")
+    if [[ ${testing} -eq 1 ]]; then
+        thischain_workdir="${thischain_workdir}_test"
+    fi
+    if [[ ! -d ${thischain_workdir} ]]; then
+        continue
+    fi
 
-	# Get short name for this chain
-	thischain_name="$(g2p_chain_shortname.sh ${d} ${testing})"
+    # Get short name for this chain
+    thischain_name="$(g2p_chain_shortname.sh ${d} ${testing})"
 
-	actdir="${d}/actual"
-	if [[ ! -d "${actdir}" ]]; then
-		echo "actdir not found: ${PWD}/${actdir})"
-		exit 13
-	fi
-	ssp_list=$(ls -d "${actdir}"/ssp[0-9][0-9][0-9] | sed "s@${actdir}/@@g") 
-	s=0
-	latest_job=""
-	latest_actual_job="-1"
-	for ssp in ${ssp_list}; do
-		s=$((s+1))
-		if [[ ${ssp} == $(echo ${ssp_list} | grep -oE '[^ ]+$') ]]; then
-			islast_act=1
-		else
-			islast_act=0
-		fi
+    actdir="${d}/actual"
+    if [[ ! -d "${actdir}" ]]; then
+        echo "actdir not found: ${PWD}/${actdir})"
+        exit 13
+    fi
+    ssp_list=$(ls -d "${actdir}"/ssp[0-9][0-9][0-9] | sed "s@${actdir}/@@g") 
+    s=0
+    latest_job=""
+    latest_actual_job="-1"
+    for ssp in ${ssp_list}; do
+        s=$((s+1))
+        if [[ ${ssp} == $(echo ${ssp_list} | grep -oE '[^ ]+$') ]]; then
+            islast_act=1
+        else
+            islast_act=0
+        fi
 
-		# Get potential column headers, if necessary
-		potdir="${d}/potential/${ssp}/"
-		if [[ ! -d "${potdir}" ]]; then
-			echo "potential-run directory not found: ${PWD}/${potdir}"
-			exit 11
-		fi
-		if [[ "${pot_col_heads}" == "" ]]; then
-			pot_col_heads="$(echo $(ls "${potdir}") | sed "s/ /,/g")"
-			pot_col_heads="$(echo ${pot_col_heads} | sed "s/,20/,/g" | sed "s/-20/-/g")"
-		fi
+        # Get potential column headers, if necessary
+        potdir="${d}/potential/${ssp}/"
+        if [[ ! -d "${potdir}" ]]; then
+            echo "potential-run directory not found: ${PWD}/${potdir}"
+            exit 11
+        fi
+        if [[ "${pot_col_heads}" == "" ]]; then
+            pot_col_heads="$(echo $(ls "${potdir}") | sed "s/ /,/g")"
+            pot_col_heads="$(echo ${pot_col_heads} | sed "s/,20/,/g" | sed "s/-20/-/g")"
+        fi
 
-		# Check historical period, if necessary
-		if [[ $s -eq 1 ]]; then
-			homedir_rel="${d}/actual/hist"
-			thisline="${thischain_name} "
-			check_jobs ${thischain_name}_hist
-		else
-			thisline=" :"
-		fi
+        # Check historical period, if necessary
+        if [[ $s -eq 1 ]]; then
+            homedir_rel="${d}/actual/hist"
+            thisline="${thischain_name} "
+            check_jobs ${thischain_name}_hist
+        else
+            thisline=" :"
+        fi
 
         # Get future-actual periods
         futureactdirs=$(ls -d "${d}/actual/${ssp}_"*)
@@ -299,7 +299,7 @@ for d in ${dirlist}; do
             exit 1
         fi
 
-		# Check future-actual periods
+        # Check future-actual periods
         x=0
         get_act_col_heads=0
         if [[ "${act_col_heads}" == "" ]]; then
@@ -311,25 +311,25 @@ for d in ${dirlist}; do
                 act_col_heads="${act_col_heads}ACT${x},"
             fi
 
-    		homedir_rel="${d_act}"
+            homedir_rel="${d_act}"
             if [[ $x -eq 1 ]]; then
                 thisline="${thisline} ${ssp/ssp/}"
             fi
             check_jobs ${thischain_name}_$(basename ${d_act})_
-    		if [[ ${latest_job} -gt ${latest_actual_job} ]]; then
-    			latest_actual_job=${latest_job}
-    		fi
+            if [[ ${latest_job} -gt ${latest_actual_job} ]]; then
+                latest_actual_job=${latest_job}
+            fi
         done
 
-		# Check potential periods
-		pot_list=$(ls "${potdir}" | cut -d' ' -f1-2)
-		for pot in ${pot_list}; do
-			homedir_rel="${d}/potential/${ssp}/${pot}"
-			check_jobs ${thischain_name}_${pot}
-		done
+        # Check potential periods
+        pot_list=$(ls "${potdir}" | cut -d' ' -f1-2)
+        for pot in ${pot_list}; do
+            homedir_rel="${d}/potential/${ssp}/${pot}"
+            check_jobs ${thischain_name}_${pot}
+        done
 
-	done
-	echo ${thisline} >> $tmpfile
+    done
+    echo ${thisline} >> $tmpfile
 done
 
 cat $tmpfile | column --table --table-columns RUNSET,HIST,SSP,${act_col_heads}${pot_col_heads} -s ": "
