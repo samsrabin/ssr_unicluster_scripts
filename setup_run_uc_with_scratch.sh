0f43939187c49e27b6c56c168144ede9be99a0b3
Sam Rabin <sam.rabin@kit.edu>
2022-05-06 00:18:50 +0200

    Generalizing some stuff in setup_run_uc_with_scratch.sh for easy changing by other users.

--- setup_run_uc_with_scratch.sh
+++ setup_run_uc_with_scratch.sh
@@ -7,7 +7,8 @@ if [[ "$#" -eq "0" ]]; then
 	exit
 fi
 
-lpjg_topdir=$HOME/lpj-guess_git-svn_20190828
+lpjg_topdir="$HOME/lpj-guess_git-svn_20190828"
+module_gnu="/home/kit/imk-ifu/lr8247/scripts_peter/module_gnu.sh"
 
 #############################################################################################
 # Function-parsing code from https://gist.github.com/neatshell/5283811
@@ -299,7 +300,6 @@ echo "finishup_partition: ${finishup_partition}"
 echo "finishup_nprocs: ${finishup_nprocs}"
 
 lpjg_dir=${lpjg_topdir}/build_$arch
-#scripts_dir=/pfs/data1/home/kit/imk-ifu/lr8247/scripts
 scripts_dir=$HOME/scripts
 binary=guess
 ########################################################################
@@ -489,7 +489,9 @@ ${reservation_txt_sbatch}
 
 set -e
 
-#/home/kit/imk-ifu/lr8247/scripts_peter/module_gnu.sh
+# Should basically contain what's in 
+# ${module_gnu}
+
 module unload \$(module -t list 2>&1 | grep "tools\|app\|io\|compiler\|mpi\|lib\|math\|devel\|numlib\|cae\|chem\|system")
 module load compiler/gnu mpi/openmpi
 module list
@@ -568,20 +570,20 @@ if [[ ${submit} -eq 1 ]]; then
 	echo "Submitting..."
     date
 	pushd "${rundir_top}" 1>/dev/null 2>&1
-	source ~/scripts_peter/module_gnu.sh 1>/dev/null 2>&1
+	source "${module_gnu}" 1>/dev/null 2>&1
 	./startguess.sh
 	popd 1>/dev/null 2>&1
 else
 	echo "To submit:"
 	echo "cd $rundir_top"
-	echo "source ~/scripts_peter/module_gnu.sh 1>/dev/null 2>&1"
+	echo "source ${module_gnu} 1>/dev/null 2>&1"
 	echo "./startguess.sh"
 	
 	if [[ ${dev} -eq 1 ]]; then
 		echo " "
 		echo "To submit interactively:"
 		echo "cd $rundir_top"
-		echo "source ~/scripts_peter/module_gnu.sh 1>/dev/null 2>&1"
+		echo "source ${module_gnu} 1>/dev/null 2>&1"
 		echo "salloc --partition $queue -N $nnodes -n $nprocess --ntasks-per-core ${tasks_per_core} -t $walltime ${reservation_txt} submit.sh" 
 	fi
 fi
