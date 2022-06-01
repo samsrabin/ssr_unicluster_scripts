ba6d63288d33a46db7e4917d091307f620973ab1
Sam Rabin <sam.rabin@kit.edu>
2022-06-01 23:47:17 +0200

    Corrected a call of g2p_get_state_path_absolute.sh in g2p_setup.sh.

--- g2p_setup.sh
+++ g2p_setup.sh
@@ -330,7 +330,7 @@ for thisSSP in ${ssp_list}; do
     # I.e., -L flag
     # Would need to ensure that it's ONLY used for first part of future runs.
     state_path=""
-    state_path_absolute=$(get_state_path_absolute.sh "${rundir_top}" "${state_path_absolute}")
+    state_path_absolute=$(g2p_get_state_path_absolute.sh "${rundir_top}" "${state_path_absolute}")
     state_path_thisSSP="${state_path_absolute}_${thisSSP}"
     mkdir -p ${state_path_thisSSP}
     pushd ${state_path_thisSSP} 1>/dev/null
