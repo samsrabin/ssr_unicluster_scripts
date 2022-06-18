d5bfae16b860b3b4d9fcbf10b0735cd20ae3c9f2
Sam Rabin <sam.rabin@kit.edu>
2022-06-18 02:28:51 +0200

    Messaging correction in lsf_get_basename.sh.

--- lsf_get_basename.sh
+++ lsf_get_basename.sh
@@ -4,7 +4,7 @@
 thisbasename=$(echo $PWD | { grep -oE "/[a-zA-Z0-9\-]+_[a-zA-Z0-9\-]+_[a-zA-Z0-9\-]+_[a-zA-Z0-9\-]+/" || true; } | tail -n 1 | sed "s@/@@g")
 
 if [[ "${thisbasename}" == "" ]]; then
-	>&2 echo "g2p_get_basename.sh: No matches found"
+	>&2 echo "lsf_get_basename.sh: No matches found"
 	>&2 echo PWD $PWD
 	exit 1
 fi
