12f04a50eeb76bfe54c73d6d9dc7d9cf8810c60b
Sam Rabin <sam.rabin@kit.edu>
2022-11-04 21:50:58 +0100

    Copied some lsf_ scripts to rc_ versions.

---
+++ rc_1_acthist.sh
@@ -0,0 +1,147 @@
+if [[ "${act_restart_year}" == "" && ${separate_spinup} -eq 1 ]]; then
+    lastsaveyear=${firsthistyear}
+else
+    lastsaveyear=$(echo ${save_years} | awk '{print $NF}')
+fi
+
+# Possibly skip this run
+if [[ ( "${act_restart_year}" == "" && ${first_act_y1} -gt ${hist_y1} ) || ( "${act_restart_year}" != "" && ${act_restart_year} -lt ${first_act_y1} ) ]]; then
+    # Set up for next historical run or finishup, if any
+    act_restart_year=${lastsaveyear}
+    continue
+fi
+
+# Get lasthistyear
+echo save_years $save_years;
+lasthistyear=$((lastsaveyear - 1))
+firstsaveyear=$(echo ${save_years} | cut -d" " -f1)
+do_break=0
+if [[ ${last_hist_year} -gt ${last_year_act_hist} ]]; then
+    echo "Warning: Some historical-period save_year (${lastsaveyear}) implies a run outside historical period (${last_year_act_hist})."
+    echo "         Ignoring, and ending historical run(s) in ${last_year_act_hist}."
+    lasthistyear=$((last_year_act_hist))
+do_break=1
+fi
+
+# Set up directory
+if [[ "${act_restart_year}" == "" ]]; then
+    firstyear_thisrun="spin"
+else
+    firstyear_thisrun=${act_restart_year}
+fi
+theseYears="${firstyear_thisrun}-${lasthistyear}"
+dir_acthist="actual/hist_${theseYears}"
+
+echo "#############################"
+echo "### ${dir_acthist} ###"
+echo "#############################"
+
+if [[ ${do_fu_only} -eq 1 ]]; then
+
+    made_home_dir=0
+    if [[ ! -d ${dir_acthist} ]]; then
+        made_home_dir=1
+        mkdir ${dir_acthist}
+        home_dir_realpath="$(realpath ${dir_acthist})"
+    fi
+    pushdq ${dir_acthist}
+
+else
+
+    # Archive existing directory, if needed
+    if [[ -d "${dir_acthist}" ]]; then
+        archive_acthist="${dir_acthist}.$(date "+%Y-%m-%d-%H%M%S").tar"
+        echo "Archiving existing $(pwd)/${dir_acthist} as ${archive_acthist}"
+        tar -cf "${archive_acthist}" "${dir_acthist}"
+        rm -rf "${dir_acthist}"
+    fi
+
+    # Make run directory from template
+    cp -a template "${dir_acthist}"
+    pushdq ${dir_acthist}
+
+    # Replace placeholder values from template
+    sed -i "s/UUUU/${lasthistyear}/" main.ins    # lasthistyear
+    if [[ "${act_restart_year}" == "" ]]; then
+        sed -iE "s/^\s*restart_year/\!restart_year/g" main.ins
+    else
+        sed -iE "s/^\!restart_year VVVV/restart_year ${act_restart_year}/g" main.ins
+        sed -i "s/VVVV/${act_restart_year}/" main.ins    # restart_year
+        sed -i "s/restart 0/restart 1/g" main.ins
+    fi
+    sed -i "s/WWWW/\"${save_years}\"/" main.ins    # save_years
+    sed -i "s/XXXX/${last_LUyear_past}/" landcover.ins    # XXXXpast_YYYYall_LU.txt
+    sed -i "s/YYYY/${last_LUyear_all}/" landcover.ins    # XXXXpast_YYYYall_LU.txt
+    sed -iE "s/^\s*first_plut_year/\!first_plut_year/g" landcover.ins
+    sed -i "s/co2_ssp585_annual_2015_2100.txt/co2_historical_annual_1850_2014.txt/g" main.ins
+    sed -i "s/population-density_3b_2015soc_30arcmin_annual_1601_2100.lpjg.nc/population-density_3b_histsoc_30arcmin_annual_1850_2014.lpjg.nc/g" main.ins
+    sed -i "s/Effectively 2015soc/histsoc/g" main.ins
+    sed -i "s/2015soc/histsoc/g" main.ins
+
+    set " " 
+
+    
+    # Get gridlist
+    gridlist=$(get_param.sh ${topinsfile} "file_gridlist")
+    [[ "${gridlist}" == "get_param.sh_FAILED" ]] && exit 1
+    if [[ "${gridlist}" == "" ]]; then
+        echo "Unable to parse gridlist from ${topinsfile} and its dependencies"
+        exit 1
+    fi
+
+fi # if do_fu_only else
+
+# Set up rundir_top
+if [[ "${rundir_top}" == "" ]]; then
+    rundir_top=$(lsf_get_rundir_top.sh ${istest} 0)
+    if [[ "${rundir_top}" == "" ]]; then
+        echo "Error finding rundir_top; exiting."
+        exit 1
+    fi
+    if [[ ${do_fu_only} -eq 0 ]]; then
+        mkdir -p "${rundir_top}"
+    fi
+fi
+
+# Delete restart files?
+if [[ ${act_restart_year} -le ${hist_y1} || ${act_restart_year} -eq ${future_y1} ]]; then
+    delete_state_arg=
+else
+    delete_state_arg="--delete-state-year ${act_restart_year}"
+    if [[ "${latest_pot_jobnum}" != "" ]]; then
+        echo "WILL DELETE ${act_restart_year} STATE IF POTENTIAL JOB ${latest_pot_jobnum} FINISHED OK"
+        delete_state_arg+=" --delete-state-year-if-thisjob-ok ${latest_pot_jobnum}"
+    else
+        echo "WILL DELETE ${act_restart_year} STATE"
+    fi
+fi
+
+# Submit historical run or finishup
+state_path="$(cd ..; lsf_get_rundir_top.sh ${istest} 0)/states"
+this_prefix="${prefix}_hist"
+ispot=0
+do_setup ${walltime_hist} ${ispot} ${delete_state_arg}
+
+if [[ ${do_fu_only} -eq 0 ]]; then
+    arr_job_name+=("act-hist_${theseYears}")
+    previous_act_jobnum=$(get_latest_run)
+    if [[ "${submit}" != "" ]]; then
+        arr_job_num+=( ${previous_act_jobnum} )
+    fi
+    arr_y1+=(0) # nonsense
+    arr_yN+=(${lasthistyear})
+elif [[ ${made_home_dir} -eq 1 ]]; then
+    rmdir "${home_dir_realpath}"
+fi
+
+# Set up for next historical run or finishup, if any
+act_restart_year=${lastsaveyear}
+
+echo " "
+echo " "
+popdq
+
+if [[ ${do_break} -eq 1 ]]; then
+    break
+fi
+
---
+++ rc_setup.sh
@@ -0,0 +1,371 @@
+#!/bin/bash
+set -e
+
+reservation=""
+#reservation="-r landsymm-project"
+realinsfile="main.ins"
+#testinsfile="main_test2.ins"; testnproc=1
+testinsfile="main_test1_fast.ins"; testnproc=1
+testinsfile="test3x40.ins"; testnproc=40
+#testinsfile="main_test2x2.ins"; testnproc=2
+#testinsfile="main_test160x3.ins"; testnproc=160
+inputmodule="cfx"
+walltime_hist="48:00:00"
+walltime_fut="48:00:00"  # Should take around ??? hours
+walltime_minutes_max=4320
+round_walltime_to_next=30        # minutes
+walltime_pot_minutes_minimum=90  # 160 processes, Unicluster
+walltime_pot_minutes_peryr=3.4   # 160 processes, Unicluster
+hist_y1=1850
+future_y1=2015
+maxNstates=3
+future_yN=2100 # Because last year of emulator output is 2084
+Nyears_getready=2
+
+#############################################################################################
+# Function-parsing code from https://gist.github.com/neatshell/5283811
+
+script="lsf_setup.sh"
+function usage {
+    echo " "
+    echo -e "usage: $script [-t]\n"
+}
+
+# Set default values for non-positional arguments
+arch="landsymm-dev-forestry"
+istest=0
+arg_yes_fu=0
+arg_no_fu=0
+do_fu_only=0
+submit=""
+dirForPLUM=""
+dependency_in=""
+actual_only=0
+potential_only=0
+nproc=160
+ssp_list="hist ssp126 ssp370 ssp585"
+Nyears_pot=99999
+#Nyears_pot=100
+first_pot_y1=1850
+first_act_y1=${hist_y1}
+last_pot_y1=999999999
+pot_step=20
+pot_yN=2100
+# Handle possible neither/both specs here
+mem_per_node_default=90000 # MB
+mem_per_node=-1 # MB
+mem_per_cpu_default=500 # MB
+mem_per_cpu=-1 # MB
+
+# Args while-loop
+while [ "$1" != "" ];
+do
+    case $1 in
+        -a  | --actual-only)
+            actual_only=1
+            ;;
+        -n  | --nproc) shift
+            nproc="$1"
+            ;;
+        -p  | --potential-only)
+            potential_only=1
+            ;;
+        -s  | --submit)
+            submit="--submit"
+            ;;
+        -t  | --test)
+            istest=1
+            ;;
+        --arch) shift
+            arch="$1"
+            ;;
+        --fu)
+            arg_yes_fu=1
+            ;;
+        --no-fu)
+            arg_no_fu=1
+            ;;
+        --fu-only)
+            do_fu_only=1
+            ;;
+        --dirForPLUM)  shift
+            dirForPLUM="$1"
+            ;;
+        --ssp-list)  shift
+            ssp_list="$1"
+            ;;
+        --mem-per-node)  shift
+            mem_per_node=$1
+            ;;
+        --mem-per-cpu)  shift
+            mem_per_cpu=$1
+            ;;
+        --nyears-pot)  shift
+            Nyears_pot=$1
+            ;;
+        --first-y1-pot)  shift
+            first_pot_y1=$1
+            ;;
+        --last-y1-pot)  shift
+            last_pot_y1=$1
+            ;;
+        --yN-pot)  shift
+            pot_yN=$1
+            ;;
+        --step-pot)  shift
+            pot_step=$1
+            ;;
+        --first-y1-act)  shift
+            first_act_y1=$1
+            ;;
+        -d | --dependency)  shift
+            dependency_in="-d $1"
+            ;;
+        *)
+            echo "$script: illegal option $1"
+            usage
+            exit 1 # error
+            ;;
+    esac
+    shift
+done
+
+if [[ ${first_act_y1} -lt ${hist_y1} ]]; then
+    echo "--first-y1-act (${first_act_y1}) must be >= ${hist_y1}"
+    exit 1
+fi
+
+# Process memory specification
+. "${HOME}/scripts/process_slurm_mem_spec.sh"
+
+if [[ "${dirForPLUM}" != "" && ! -d "${dirForPLUM}" ]]; then
+    echo "dirForPLUM does not exist: ${dirForPLUM}"
+    exit 1
+fi
+
+# Do finishup or no?
+if [[ ${do_fu_only} -eq 1 ]]; then
+    if [[ ${arg_no_fu} == "1" ]]; then
+        echo "Both --fu-only and --no-fu specified; choose one."
+        exit 1
+    fi
+    do_fu=1
+elif [[ ${arg_no_fu} == "1" && ${arg_yes_fu} == "1" ]]; then
+    if [[ ${istest} -eq 1 ]]; then
+        echo "Both --fu and --no-fu specified. Using dev default of NO finishup."
+        do_fu=0
+    else
+        echo "Both --fu and --no-fu specified. Using non-dev default of YES finishup."
+        do_fu=1
+    fi
+elif [[ ${arg_no_fu} == "1" ]]; then
+    do_fu=0
+elif [[ ${istest} -eq 1 ]]; then
+    if [[ ${arg_yes_fu} == "1" ]]; then
+        do_fu=1
+    else
+        do_fu=0
+    fi
+elif [[ ${arg_no_fu} == "1" ]]; then
+    do_fu=0
+else
+    do_fu=1
+fi
+
+#############################################################################################
+
+# Process test vs. real thing
+if [[ ${istest} -eq 1 ]]; then
+    topinsfile=${testinsfile}
+    walltime_hist="30:00"
+    walltime_fut="30:00"
+    nproc=${testnproc}
+    ppfudev="--dev"
+    if [[ $do_fu_only -eq 1 ]]; then
+        ppfudev="--dev --fu_only"
+    elif [[ $do_fu -eq 1 ]]; then
+        ppfudev="--dev --fu"
+    fi
+    reservation=""
+#    maxNstates=999
+else
+    topinsfile=${realinsfile}
+    if [[ $do_fu_only -eq 1 ]]; then
+        if [[ $do_fu -eq 0 ]]; then
+            echo "Both --fu-only and --no-fu specified; choose one."
+            exit 1
+        fi
+        ppfudev="--fu_only"
+    fi
+    if [[ $do_fu -eq 0 ]]; then
+        ppfudev="--no_fu"
+    fi
+fi
+
+# Are we actually submitting historical period?
+if [[ $(echo ${ssp_list} | cut -f1 -d" ") == "hist" ]]; then
+    do_hist=1
+else
+    do_hist=0
+fi
+
+# We don't need hist in the SSP list anymore
+if [[ ${do_hist} -eq 1 ]]; then
+    ssp_list="$(echo ${ssp_list} | sed "s/hist//")"
+fi
+
+# Future period?
+if [[ "${ssp_list/ /}" == "" ]]; then
+    do_future=0
+else
+    do_future=1
+fi
+
+
+#############################################################################################
+
+. lsf_get_years.sh
+. lsf_helper_functions.sh
+
+#############################################################################################
+
+echo " "
+date
+echo " "
+
+while [[ ! -d template ]]; do
+    cd ../
+    if [[ "$PWD" == "/" ]]; then
+        echo "lsf_setup.sh must be called from a (subdirectory of a) directory that has a template/ directory"
+        exit 1
+    fi
+done
+
+
+# Set up job arrays
+arr_job_name=()
+arr_job_num=()
+arr_y1=()
+arr_yN=()
+
+# Get job name prefix
+prefix="$(lsf_chain_shortname.sh $(basename ${PWD}) ${istest})"
+
+
+#########################################
+### Set up "actual" historical run(s) ###
+#########################################
+
+previous_act_jobnum=
+mkdir -p actual
+act_restart_year=
+
+did_resume_pre2015pots=()
+for thisSSP in ${ssp_list}; do
+    if [[ ${first_pot_y1} -le ${future_y1} ]]; then
+        did_resume_pre2015pots+=(0)
+    else
+        did_resume_pre2015pots+=(-1)
+    fi
+done
+
+if [[ "${save_years_lines}" == "" ]]; then
+    if [[ ${actual_only} -eq 1 ]]; then
+        echo "-a/--actual only, but save_years_lines is empty" >&2
+    else
+        echo "If you only want potential runs, specify -p/--potential-only so that save_years_lines is correctly filled." >&2
+    fi
+    exit 1
+fi
+
+# Set up/start a run for each set of save years
+while IFS= read -r save_years; do
+
+    # First year in this this determines whether we're in the historical
+    # period or not
+    first_save_year=$(echo ${save_years} | cut -d" " -f1)
+
+    if [[ ${first_save_year} != "" && ${first_save_year} -le ${hist_yN} ]]; then
+
+        # Sanity check
+        if [[ ${do_hist} == 0 ]]; then
+            echo "do_hist 0 but save_years ${save_years}"
+            exit 1
+        fi
+
+        # Set up/submit actual historical run(s)
+        if [[ ${potential_only} -eq 0 ]]; then
+            if [[ "${dependency_on_latest_potset}" != "" ]]; then
+                dependency="${dependency_on_latest_potset}"
+            else
+                dependency="${dependency_in}"
+                if [[ ${previous_act_jobnum} != "" ]]; then
+                    dependency+=" -d ${previous_act_jobnum}"
+                fi
+            fi # if this is the first future-actual
+            thisSSP=""
+            . lsf_1_acthist.sh
+        fi
+
+        # Set up/submit potential historical run(s)
+        if [[ ${actual_only} -eq 0 ]]; then
+            pot_years="${save_years}"
+            thisSSP="hist"
+            resume_pre2015pots=0
+            echo lsf_setup_potential_loop.sh A
+            . lsf_setup_potential_loop.sh
+            save_years=${future_y1}
+        fi
+    else
+
+        s=-1
+        for thisSSP in ${ssp_list}; do
+            s=$((s + 1))
+            if [[ "${thisSSP}" != "hist" && "${thisSSP:0:3}" != "ssp" ]]; then
+                thisSSP="ssp${thisSSP}"
+            fi
+            this_prefix="${prefix}_${thisSSP}"
+
+            # Start 2015-resuming potential runs, if needed
+            first_pot_y1=$(echo ${list_pot_y1_future} | cut -d" " -f1)
+            if [[ ( ( ${first_save_year} != "" && ${first_pot_y1} -lt ${first_save_year} ) || ${potential_only} -eq 1 ) && ${did_resume_pre2015pots[s]} == 0 && ${thisSSP} != "hist" ]]; then
+                resume_pre2015pots=1
+                echo lsf_setup_potential_loop.sh B
+                . lsf_setup_potential_loop.sh
+                did_resume_pre2015pots[s]=1
+            fi
+
+            if [[ ${potential_only} -eq 0 && ${do_future_act} -eq 1 ]]; then
+                pushdq "actual"
+                if [[ "${dependency_on_latest_potset}" != "" ]]; then
+                    dependency="${dependency_on_latest_potset}"
+                else
+                    dependency="${dependency_in}"
+                    if [[ ${previous_act_jobnum} != "" ]]; then
+                        dependency+=" -d ${previous_act_jobnum}"
+                    fi
+                fi # if this is the first future-actual
+
+                . lsf_1_actfut.sh
+                popdq
+            fi # if doing future-actual
+
+            if [[ ${actual_only} -eq 0 && "${save_years}" != "" ]]; then
+                pot_years="${save_years}"
+                resume_pre2015pots=0
+                echo lsf_setup_potential_loop.sh C
+                . lsf_setup_potential_loop.sh
+            fi
+        done # loop through SSPs
+    fi # whether in historical or future period
+done <<< ${save_years_lines}
+
+
+echo arr_job_name ${arr_job_name[@]}
+echo arr_job_num ${arr_job_num[@]}
+echo arr_y1 ${arr_y1[@]}
+echo arr_yN ${arr_yN[@]}
+
+squeue -o "%10i %.7P %.35j %.10T %.10M %.9l %.6D %.16R %E" -S JOBID | sed "s/unfulfilled/unf/g"
+
+exit 0
