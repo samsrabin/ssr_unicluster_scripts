68fb6cddd6360c33dd88111f45dd422e6202c57c
Sam Rabin <sam.rabin@gmail.com>
2022-11-30 18:59:57 +0100

    Avoid "UCX is unable to handle VM_UNMAP event" warnings in *_1run.sh scripts.

--- lsf_setup_1run.sh
+++ lsf_setup_1run.sh
@@ -547,6 +547,10 @@ if [[ \$diagnostics -eq 1 ]]; then
     fi
 fi
 
+# Trying to avoid these warnings:
+# common_ucx.c:162  Warning: UCX is unable to handle VM_UNMAP event. This may cause performance degradation or data corruption. Pls try adding --mca opal_common_ucx_opal_mem_hooks 1 to mpirun/oshrun command line to resolve this issue.
+mpirun_options+=" --mca opal_common_ucx_opal_mem_hooks 1"
+
 echo " "
 echo "mpirun_options:"
 echo $mpirun_options
