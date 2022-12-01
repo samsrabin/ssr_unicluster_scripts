be6e8725c5d345533134d9921fdbfee6e0dcda0e
Sam Rabin <sam.rabin@gmail.com>
2022-12-01 22:55:45 +0100

    lsf_get_basename.sh now includes . as a word character.

--- lsf_get_basename.sh
+++ lsf_get_basename.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Find the first directory above (or including) this one that is four_under-score_separated_words 
-thisbasename=$(echo $PWD | { grep -oE "/[a-zA-Z0-9\-]+_[a-zA-Z0-9\-]+_[a-zA-Z0-9\-]+_[a-zA-Z0-9\-]+/" || true; } | tail -n 1 | sed "s@/@@g")
+thisbasename=$(echo $PWD | { grep -oE "/[a-zA-Z0-9\-\.]+_[a-zA-Z0-9\-\.]+_[a-zA-Z0-9\-\.]+_[a-zA-Z0-9\-\.]+/" || true; } | tail -n 1 | sed "s@/@@g")
 
 if [[ "${thisbasename}" == "" ]]; then
 	>&2 echo "lsf_get_basename.sh: No matches found"
