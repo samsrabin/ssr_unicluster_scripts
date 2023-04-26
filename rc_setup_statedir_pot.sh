31448d8e3c9723cf0b270047912b07c1a4492d9f
Sam Rabin <sam.rabin@gmail.com>
2023-04-27 01:11:17 +0200

    Correct state dir linking for post-2014 SAI ssp245 pot runs.

--- rc_setup_statedir_pot.sh
+++ rc_setup_statedir_pot.sh
@@ -1,9 +1,11 @@
 
+#echo " "
+#echo rc_setup_statedir_pot.sh
 #echo state_path_thisSSP $state_path_thisSSP
-#exit 1
 
 if [[ ${runtype} == "sai" && "${thisSSP}" != "hist" ]]; then
     state_path_thisSSP+=".${ensemble_member_fut}"
+#echo state_path_thisSSP $state_path_thisSSP
 fi
 
 mkdir -p ${state_path_thisSSP}
@@ -15,10 +17,15 @@ fi
 
 if [[ ${save_state_sai2035} -eq 1 ]]; then
     ln -s ../../actual/states_ssp245.${ensemble_member_fut}/${y0}
-elif [[ "${histname}" != "hist" && "${thisSSP}" != "${histname}" ]]; then
-    ln -s ../../actual/states_${histname}/${y0}
+elif [[ "${histname}" != "hist" && ${y0} -gt ${hist_y1} ]]; then
+    if [[ "${thisSSP}" != "${histname}" && ${y0} -gt ${future_y1} ]]; then
+        ln -s ../../actual/states_${histname}/${y0}
+    else
+        ln -s ../../actual/states_${thisSSP}/${y0}
+    fi
 else
     ln -s ../../actual/states/${y0}
 fi
+
 popd 1>/dev/null
 
