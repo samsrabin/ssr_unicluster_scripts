81d6abc53aa6128bdf28f630a1983a62f12d8b9c
Sam Rabin <sam.rabin@gmail.com>
2026-02-04 00:26:24 +0100

    setup_run_uc_with_scratch_ggcmi3: Fix state_path_absolute.

--- setup_run_uc_with_scratch_ggcmi3.sh
+++ setup_run_uc_with_scratch_ggcmi3.sh
@@ -178,7 +178,7 @@ fi
 # Parse absolute state path, if not provided
 if [[ "${state_path_absolute}" == "" ]]; then
 	topdir=$(realpath ..)
-	state_path_absolute=$(echo $topdir | sed "s@/pfs/data5@@" | sed "s@$HOME@$WORK@" | sed "s@/$topdir@@")/states
+	state_path_absolute=$(echo $topdir | sed "s@/pfs/data6@@" | sed "s@$HOME@$WORK@" | sed "s@/$topdir@@")/states
     if [[ -d "${state_path_absolute}" ]]; then
         state_path_absolute="$(realpath "${state_path_absolute}")"
     fi
